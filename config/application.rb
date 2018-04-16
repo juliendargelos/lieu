c75f29d92f0a73e0047136db1739a726695c9d3e
Julien Dargelos <julien.dargelos@me.com>
2018-04-16 14:00:10 +0200

    Configuration

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,8 @@ module Lilu
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    config.sass.preferred_syntax = :sass
+    config.generators { |g| g.javascript_engine :js }
   end
 end

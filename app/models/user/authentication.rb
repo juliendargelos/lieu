a3766911c9649e2c2bc9f06e911cf0f78f34bd35
Julien Dargelos <julien.dargelos@me.com>
2018-05-08 20:45:57 +0200

    #1 #9

--- app/models/user/authentication.rb
+++ app/models/user/authentication.rb
@@ -3,6 +3,6 @@ class User::Authentication < Authentication::Base
   has_public :pseudo
   has_private :password
 
-  validates :pseudo, presence: true
-  validates :password, presence: true
+  validates :pseudo, presence: { message: 'Nous avons besoin de ton pseudo' }
+  validates :password, presence: { message: 'Nous avons besoin de ton mot de passe' }
 end

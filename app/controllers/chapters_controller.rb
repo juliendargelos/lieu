a999281a541e88be9b245912846ba0134128be68
Julien Dargelos <julien.dargelos@me.com>
2018-05-15 13:05:14 +0200

    Better chapters resource

--- app/controllers/chapters_controller.rb
+++ app/controllers/chapters_controller.rb
@@ -1,8 +1,14 @@
 class ChaptersController < ApplicationController
   authenticates! :user
+  before_action :set_book
 
   def index
-    @chapters = Chapter.all
+    @chapters = @book.chapters
   end
 
+  private
+
+  def set_book
+    @book = Book.find params[:book_id]
+  end
 end

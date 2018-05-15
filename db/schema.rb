cff2c836256aa086246dd43e5f60c546b65bafd3
Julien Dargelos <julien.dargelos@me.com>
2018-05-15 12:27:08 +0200

    Seeds

--- db/schema.rb
+++ db/schema.rb
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 2018_05_15_091442) do
     t.integer "icon_file_size"
     t.datetime "icon_updated_at"
     t.string "author"
+    t.integer "status", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,8 @@
+puts 'Destroying books...'
+Book.destroy_all
+
+puts ''
+
 puts 'Creating books...'
 Dir[Rails.root + 'db/seeds/books/*.yml'].each do |path|
   data = YAML.load_file(path).deep_symbolize_keys!.merge! icon: File.new(Rails.root + "db/seeds/books/icons/#{File.basename path, '.yml'}.svg")
@@ -9,6 +14,38 @@ Dir[Rails.root + 'db/seeds/books/*.yml'].each do |path|
   Chapter.create data[:chapters]&.map{ |chapter| chapter.merge! book: book } || []
 
   puts ''
+end
+
+if Rails.env.development?
+  puts 'Destroying users...'
+  User.destroy_all
+
+  puts ''
+
+  data = [
+    {
+      pseudo: :juliendargelos,
+      email: 'julien.dargelos@me.com',
+      password: :juliendargelos,
+      password_confirmation: :juliendargelos
+    },
+    {
+      pseudo: :circe,
+      email: 'circegrand@gmail.com',
+      password: :motdepasse,
+      password_confirmation: :motdepasse
+    }
+  ]
+
+  puts 'Creating users...'
+  User.create data
+
+  data.each do |user|
+    puts "  Pseudo: #{user[:pseudo]}"
+    puts "  Password: #{user[:password]}"
+
+    puts ''
+  end
 end
 
 puts 'Finished.'

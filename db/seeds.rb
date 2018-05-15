6c527d6e45f6e31b5ebc38a3a6d0b0f38ffb83ec
Julien Dargelos <julien.dargelos@me.com>
2018-05-15 12:10:21 +0200

    Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,51 +1,14 @@
-Book.create [
-  {
-    title: 'Fantastique Maître Renard',
-    summary: "Trois riches fermiers : Boggis, Bunce et Bean, un gros, un petit et un maigre, tous éleveurs de volailles, méchants, vulgaires et bêtes, décident d'avoir la peau de Maître Renard et de sa petite famille. Ceux-ci sont affamés, peut-être, mais aussi très rusés. Les trois fermiers risquent d'attendre longtemps à côté de leur terrier !Un classique international, à la fois cocasse et tendre. S'il ne fallait lire qu'une seule histoire de renard et de poules, incontestablement, ce serait celle-là.",
-    number_of_chapters: 18,
-    author: 'Roald Dahl',
-    icon: File.new(__dir__ + '/seeds/fantastique-maitre-renard.svg')
-  },
-  {
-    title: 'Le Chien des mers',
-    number_of_chapters: 10,
-    summary: "Dans la famille de Jean, on est corsaire de père en fils et l'on écume les mers au service du roi. C'est plus dangereux que d'aller à l'école. A neuf ans, Jean embarque comme mousse et l'aventure commence: poursuites canonnades et abordages. « Mort aux Anglais ! » crie Jean avec ses compagnons d'armes. Mais un jour, il rentrera chez lui, à Saint-Malo, où l'attend Toinette, la « fille des fées », qui lui a juré un amour éternel. Et ils se marieront. Et il ne sera plus question de faire la guerre",
-    author: 'Marie-Aude Murais',
-    icon: File.new(__dir__ + '/seeds/le-chien-des-mers.svg')
-  },
-  {
-    title: "Journal d'un chat assassin",
-    number_of_chapters: 10,
-    summary: "Tuffy est un chat consciencieux. Il tient un journal où il consigne la liste des oiseaux maladroits venus se jeter dans ses pattes et des souris déjà mortes quand il les a trouvées... Tuffy doit en effet se justifier sans cesse : ses maîtres n'apprécient pas son boulot de chat. Le jour où il rentre avec le lapin des voisins, ça ne s'arrange pas...Un régal de mauvaise foi, doublé d'un excellent exercice pour les zygomatiques : dix éclats de rire par page garantis. Indispensable en cas de coup de cafard.",
-    author: 'Anne Fine',
-    icon: File.new(__dir__ + '/seeds/journal-chat-assassin.svg')
-  },
-  {
-    title: "Le buveur d'encre",
-    number_of_chapters: 15,
-    summary: "Le fils du libraire déteste les livres. Son passe-temps favori consiste à guetter les pickpockets, qu'il encourage en pensée à le débarrasser de ces objets encombrants et pleins de feuilles. Un jour, il surprend un curieux voleur qui, muni d'une paille, avale les mots d'un livre entier. Il s'agit d'un vampire qui boit l'encre des livres...",
-    author: 'Eric Sanvoisin',
-    icon: File.new(__dir__ + '/seeds/buveur-encre.svg')
-  },
-  {
-    title: 'Petit Poilu - T.18 Superpoilu',
-    number_of_chapters: 10,
-    summary: "Le fils du libraire déteste les livres. Son passe-temps favori consiste à guetter les pickpockets, qu'il encourage en pensée à le débarrasser de ces objets encombrants et pleins de feuilles. Un jour, il surprend un curieux voleur qui, muni d'une paille, avale les mots d'un livre entier. Il s'agit d'un vampire qui boit l'encre des livres...",
-    author: 'Céline Fraipont',
-    icon: File.new(__dir__ + '/seeds/petit-poilu.svg')
-  },
-  {
-    title: "L’arbre à frites",
-    number_of_chapters: 15,
-    summary: "Emma se moque de son petit frère en lui faisant croire à l'existence d'un mystérieux arbre à frites. Pourtant, toutes les frites ne viennent pas du congélateur... Emma va l'apprendre à ses dépens.",
-    author: 'Ghislaine Biondi',
-    icon: File.new(__dir__ + '/seeds/arbre-a-frites.svg')
-  },
-  {
-    title: 'Les chevaliers',
-    number_of_chapters: 5,
-    summary: "Qui n’a jamais rêvé de devenir chevalier ? Vous saurez tout sur ces courageux combattants qui ont juré fidélité à un seigneur : leur formation, leur équipement, la cérémonie d’adoubement, les batailles... Plongez au cœur du Moyen Âge, l’âge d’or de la chevalerie, grâce à Quelle Histoire, qui propose une initiation ludique à l'histoire de ces guerriers.",
-    author: '',
-    icon: File.new(__dir__ + '/seeds/les-chevaliers.svg')
-  }
-]
+puts 'Creating books...'
+Dir[Rails.root + 'db/seeds/books/*.yml'].each do |path|
+  data = YAML.load_file(path).deep_symbolize_keys!.merge! icon: File.new(Rails.root + "db/seeds/books/icons/#{File.basename path, '.yml'}.svg")
+
+  puts "  Creating \"#{data[:title]}\"..."
+  book = Book.create data.except(:chapters)
+
+  puts "    Creating chapters..."
+  Chapter.create data[:chapters]&.map{ |chapter| chapter.merge! book: book } || []
+
+  puts ''
+end
+
+puts 'Finished.'

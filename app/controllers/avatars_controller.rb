e9a205fd195ee844e2671b4f53af6164ba9be3dd
Circé <circegrand@gmail.com>
2018-06-07 17:21:29 +0200

    Avatar process over

--- app/controllers/avatars_controller.rb
+++ app/controllers/avatars_controller.rb
@@ -1,7 +1,28 @@
 class AvatarsController < ApplicationController
+  authenticates! :user
+
   def index
   end
-  
+
+  def create
+    @avatar = Avatar.new avatar_params.merge!(user: current_user)
+
+    if @avatar.save
+      redirect_to books_path
+    else
+      redirect_to explanations_path
+    end
+
+  end
+
   def show
   end
+
+  private
+
+  def avatar_params
+    params.require(:avatar).permit :haircut, :eyes, :face, :sweater, :mouth, :skin, :accessory
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resource :user, path: :account, except: [:index, :show]
   # resource :avatar, path_names: { edit: '/' }, only: [:edit, :udpate]
-  resources :avatars, only: :index
+  resources :avatars, only: [:index, :create]
   resources :explanations
   resource :dashboard, only: :show
   resources :readings, only: [:show, :create, :update]
